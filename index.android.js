2aed526e0d0524e04653bf41773c2f056349a3bc
JWeesner <jwees1@umbc.edu>
2017-02-02 21:41:56 -0500

    another test commit

--- index.android.js
+++ index.android.js
@@ -24,7 +24,7 @@ export default class WrestlingClock extends Component {
         </Text>
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu!!
+          Shake or press menu button for dev menu!
         </Text>
       </View>
     );
